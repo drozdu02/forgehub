12ad8599c9c93c510af91045584813505bbded17
drozdu02 <olivier.drozdowski02@gmail.com>
2026-09-09 11:19:26 +0200

    feat: User entity declaration

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller.js';
 import { UserService } from './user.service.js';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity.js';
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
