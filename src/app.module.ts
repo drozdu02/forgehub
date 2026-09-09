3da2d454cd3631c91d5c101b004224d264b72262
drozdu02 <olivier.drozdowski02@gmail.com>
2026-09-09 10:48:24 +0200

    feat: Adding and configuring TypeORM and pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,23 @@ import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { UserModule } from './user/user.module.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'forgehub',
+      password: 'forgehub',
+      database: 'forgehubdb',
+      autoLoadEntities: true,
+      synchronize: true,
+    }), UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
