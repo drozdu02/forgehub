0e2320ecaa42b7b0798f1b0b56b6bd43eb59cded
drozdu02 <olivier.drozdowski02@gmail.com>
2026-09-12 17:18:38 +0200

    feat: Adding hashed password field to the CreateUserDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 
 export class CreateUserDto {
@@ -12,6 +12,18 @@ export class CreateUserDto {
     @IsString()
     @IsEmail()
     email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(200)
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>_\-+=~`[\]\\/;'])[A-Za-z\d!@#$%^&*(),.?":{}|<>_\-+=~`[\]\\/;']{8,}$/,
+    {
+        message: "Password must unclude a small letter, big letter and a special character"
+    }
+    )
+    passwordHash: string;
+
 }
 
 
